cc0e9b5e9d3bf15744b77591dc4e0d9ec8c30869
luniemma <luniyisiemmanuel@gmail.com>
2024-05-16 17:52:44 -0400

    add description to cluster

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,7 +5,7 @@ variable "region" {
 }
 
 variable "clusterName" {
-  description = "Name of the EKS cluster"
+  description = "Name of the EKS cluster for kubernestes"
   type        = string
   default     = "vprofile-eks"
 }
